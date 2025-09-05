ddf3d02688f5832e35efc005622ee757bf924765
Prince Kumar <143206997+prince-kumar-85@users.noreply.github.com>
2025-09-05 22:35:07 +0530

    Delete cpsto_Project directory

--- cpsto_Project/backend/utils/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id, role, jti) => {
-  return jwt.sign({ id, role, jti }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "12h",
-  });
-};
-
-module.exports = { generateToken };
